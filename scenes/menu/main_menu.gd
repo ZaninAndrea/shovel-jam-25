161c8c0f5e576f44f8b102172bb6c058b740aa8b
Andrea Zanin <andrea@igloo.ooo>
2025-07-18 22:17:56 +0200

    Bottone play funzionante

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -1,8 +1,9 @@
 extends Control
 
+const SHIP_WITH_TIME_TRAVEL = preload("res://scenes/rooms/ship_with_time_travel/ShipWithTimeTravel.tscn")
 
 func _on_play_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_packed(SHIP_WITH_TIME_TRAVEL)
 
 
 func _on_settings_pressed() -> void:
--- scenes/ui/clock/clock.gd
+++ scenes/ui/clock/clock.gd
@@ -2,6 +2,7 @@
 extends Label
 
 func _ready():
+	text = "%02d:%02d:%02d" % [Clock.hour, Clock.minute, Clock.second]
 	Clock.clock.connect(update_clock_label)
 	
 func update_clock_label(hour: int, minute: int, second: int) -> void:
