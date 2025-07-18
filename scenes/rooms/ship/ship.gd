1a99aebb661412adf84e3f3345441e5ff1f168f9
Andrea Zanin <andrea@igloo.ooo>
2025-07-18 23:04:49 +0200

    Impostato layout dell'astronave

--- scenes/rooms/ship/ship.gd
+++ scenes/rooms/ship/ship.gd
@@ -1,8 +1,3 @@
 class_name Ship extends Node2D
 @onready var rooms_container: Node = $Rooms
 @export var active_room: int = 0
-
-func _input(event):
-	if event.is_action_pressed("ui_select"):
-		active_room = (active_room + 1) % rooms_container.get_child_count()
-		rooms_container.get_child(active_room).find_child("Camera2D").make_current()
