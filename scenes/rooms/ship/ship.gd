db6e123b369d9ec67075f112b424b0b97c8b4496
Andrea Zanin <andrea@igloo.ooo>
2025-07-19 17:52:39 +0200

    Aggiunta stanza centrale

--- scenes/rooms/ship/ship.gd
+++ scenes/rooms/ship/ship.gd
@@ -1,3 +1,20 @@
 class_name Ship extends Node2D
 @onready var rooms_container: Node = $Rooms
 @export var active_room: int = 0
+
+func _ready():
+	var room :Room= rooms_container.get_child(0)
+	var player :Player = _find_player()
+	
+	player.scale = Vector2.ONE
+	player.speed = room.speed;
+	player.bottom_scale = room.bottom_scale;
+	player.top_scale = room.top_scale;
+	player.top_position = room.top_position;
+
+	
+func _find_player() -> Player:
+	for node in get_tree().get_nodes_in_group("player"):
+		return node
+		
+	return null
