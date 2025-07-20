58afa2e8e88e326537a86fcde7ff675f08ad2c32
SalaA98 <alesaladue@gmail.com>
2025-07-20 18:18:24 +0200

    Migliorate le forme dei contatti nel circuito

---
+++ scenes/rooms/stanzaB/enigma_circuito.gd
@@ -0,0 +1,33 @@
+class_name EnigmaCircuit extends Area2D
+
+@onready var sphere: DroppedItem = $"../Sphere"
+@export var enigma: PackedScene = null
+
+var enigma_instance: Enigma = null
+
+func interact():
+	if enigma == null:
+		push_error("Null enigma with locked computer")
+		return
+	
+	if enigma_instance != null:
+		return
+	
+	var enigma_scene = enigma.instantiate()
+	if not enigma_scene is Enigma:
+		push_error("Enigma scene does not extend enigma")
+	
+	enigma_instance = enigma_scene
+	get_tree().current_scene.add_child(enigma_instance)
+	
+	enigma_instance.closed.connect(closed_enigma)
+	enigma_instance.solved.connect(solved_enigma)
+
+
+func closed_enigma():
+	enigma_instance.queue_free()
+	enigma_instance = null
+
+func solved_enigma():
+	enigma_instance.queue_free()
+	enigma_instance = null
