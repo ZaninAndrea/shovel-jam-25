572b7c1e1c164a495f52957855c630da3e20e623
Andrea Zanin <andrea@igloo.ooo>
2025-07-20 21:43:10 +0200

    Drop overwritten items

--- autoload/inventory/inventory.gd
+++ autoload/inventory/inventory.gd
@@ -4,9 +4,15 @@ extends Node
 var items: Array[Item] = [null, null]
 
 signal item_set(item: Item, slot:int)
+const DROPPED_ITEM = preload("res://scenes/objects/dropped_item/dropped_item.tscn")
 
 func set_item(item: Item):
 	if item.double_slot:
+		if items[0] != null:
+			drop_item(items[0], 1)
+		if items[1] != null:
+			drop_item(items[1], 2)
+		
 		# Double slot items overwrite everything
 		items[0] = item
 		items[1] = null
@@ -14,17 +20,36 @@ func set_item(item: Item):
 		# If the first slot is free use it
 		items[0] = item
 	elif items[0].double_slot:
+		drop_item(items[0], 1)
+		
 		# If the inventory contained a double slot item replace it
 		items[0] = item
 	elif items[1] == null:
 		# If the second slot is free use it
 		items[1] = item
 	else:
-		# If all slots are used up replace the first slot
-		items[0] = item
+		drop_item(items[0], 1)
+		# If all slots push the first item out of the queue
+		items[0] = items[1]
+		items[1] = item
 	
 	item_set.emit(items[0], 1)
 	item_set.emit(items[1], 2)
+	
+func drop_item(item: Item, slot: int):
+	var player = _find_player()
+	
+	var drop = DROPPED_ITEM.instantiate()
+	player.get_parent().add_child(drop)
+	drop.global_position = player.global_position + Vector2((slot - 1.5)*200, 0)
+
+	drop.item = item
+	
+func _find_player() -> Player:
+	for node in get_tree().get_nodes_in_group("player"):
+		return node
+		
+	return null
 
 func remove_item(slot: int):
 	items[slot-1] = null
