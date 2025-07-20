49d2226f3bbd65eead3561421ca2133477b7dc6b
SalaA98 <alesaladue@gmail.com>
2025-07-20 11:21:26 +0200

    Pinpad scena completa

--- scenes/ui/pinpad/pinpad.gd
+++ scenes/ui/pinpad/pinpad.gd
@@ -1,9 +1,13 @@
 extends Control
 
 var textures = {
-	"char1": preload("res://assets/sprites/Pinpad/char1.png"),
-	"char2": preload("res://assets/sprites/Pinpad/char2.png"),
-	"char3": preload("res://assets/sprites/Pinpad/char3.png")
+	"char1": preload("res://assets/sprites/Pinpad/Simbolo1.png"),
+	"char2": preload("res://assets/sprites/Pinpad/Simbolo2.png"),
+	"char3": preload("res://assets/sprites/Pinpad/Simbolo3.png"),
+	"char4": preload("res://assets/sprites/Pinpad/Simbolo4.png"),
+	"char5": preload("res://assets/sprites/Pinpad/Simbolo5.png"),
+	"char6": preload("res://assets/sprites/Pinpad/Simbolo6.png"),
+	"char7": preload("res://assets/sprites/Pinpad/Simbolo7.png")
 }
 
 var solution: Array[String] = [
@@ -16,10 +20,10 @@ var solution: Array[String] = [
 var current_input: Array[String] = []
 
 @onready var slots: Array[TextureRect] = [
-	$Display/Slot1,
-	$Display/Slot2,
-	$Display/Slot3,
-	$Display/Slot4
+	$Slot1,
+	$Slot2,
+	$Slot3,
+	$Slot4
 ]
 
 
@@ -51,3 +55,29 @@ func _on_texture_button_2_pressed() -> void:
 
 func _on_texture_button_3_pressed() -> void:
 	on_button_pressed("char3")
+
+
+func _on_texture_button_4_pressed() -> void:
+	on_button_pressed("char4")
+
+
+func _on_texture_button_5_pressed() -> void:
+	on_button_pressed("char5")
+
+
+func _on_texture_button_6_pressed() -> void:
+	on_button_pressed("char6")
+
+
+func _on_texture_button_7_pressed() -> void:
+	on_button_pressed("char7")
+
+
+func _on_back_button_pressed() -> void:
+	print("go back")
+
+
+func _on_cancel_pressed() -> void:
+	if current_input.size() >= 1:
+		current_input.pop_back()
+		slots[current_input.size()].texture = null
